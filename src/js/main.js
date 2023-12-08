86ae6464d014d85a659b8f3a4a1b9b667812cc40
Mikhail <miwanya.spb@gmail.com>
2023-12-08 03:44:35 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,69 @@
 import '../scss/style.scss';
 import '../scss/product.scss';
+import '../scss/catalog.scss';
+import { animate } from "motion"
 
-const container = document.querySelector('.section_preview_slider');
-const prevButton = document.querySelector('.arrow_left_button');
-const nextButton = document.querySelector('.arrow_right_button');
 
-nextButton.addEventListener('click', function () {
-    checkScrollPosition();
-    scrollNext();
-    checkScrollPosition();
+window.addEventListener('DOMContentLoaded', () => {
+    console.log(window.location.href)
+    if (window.location.href === 'http://localhost:5173/product.html') {
+        const container = document.querySelector('.section_preview_slider');
+        const prevButton = document.querySelector('.arrow_left_button');
+        const nextButton = document.querySelector('.arrow_right_button');
+        nextButton.addEventListener('click', function () {
+            checkScrollPosition();
+            scrollNext();
+            checkScrollPosition();
 
-});
+        });
 
-prevButton.addEventListener('click', function () {
-    checkScrollPosition();
-    scrollPrev();
-    checkScrollPosition();
-});
+        prevButton.addEventListener('click', function () {
+            checkScrollPosition();
+            scrollPrev();
+            checkScrollPosition();
+        });
 
-function checkScrollPosition() {
-    if (Math.abs(container.scrollLeft) < 1) {
-        prevButton.style.opacity = '0';
+        function checkScrollPosition() {
+            if (Math.abs(container.scrollLeft) < 1) {
+                prevButton.style.opacity = '0';
+            } else {
+                prevButton.style.opacity = '1';
+            }
+
+            if (Math.abs(container.scrollLeft + container.offsetWidth - container.scrollWidth) < 2) {
+                nextButton.style.opacity = '0';
+            } else {
+                nextButton.style.opacity = '1';
+            }
+        }
+
+        function scrollNext() {
+            container.scrollLeft += window.innerWidth;
+        }
+
+
+        function scrollPrev() {
+            container.scrollLeft -= window.innerWidth;
+        }
+
+    }
+})
+
+let burgerOpen = false;
+
+let burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+    if (burgerOpen) {
+        animate('.menu_wrapper', { opacity: 0, transform: 'translateY(0)' });
+        animate('.menu', { opacity: 0, transform: 'translateY(-500px)' }, { duration: 0.3, easing: "ease-in-out" }).finished.then(()=> {
+            burgerOpen = false;
+        })
+
     } else {
-        prevButton.style.opacity = '1';
+        animate('.menu_wrapper', { opacity: 1, transform: 'translateY(0)' });
+        animate('.menu', {  opacity: 1, transform: 'translateY(0)'}, { duration: 0.3, easing: "ease-in-out" }).finished.then(()=> {
+            burgerOpen = true;
+        })
     }
 
-    if (Math.abs(container.scrollLeft + container.offsetWidth - container.scrollWidth) < 2) {
-        nextButton.style.opacity = '0';
-    } else {
-        nextButton.style.opacity = '1';
-    }
-}
-
-function scrollNext() {
-    container.scrollLeft += window.innerWidth;
-}
-
-
-function scrollPrev() {
-    container.scrollLeft -= window.innerWidth;
-}
-
-
+})
